5b16f963c5dafdb693047f774d2742c88edb77c7
Jon Gomez <jgomez@brownsteingroup.com>
2021-06-21 17:32:37 -0400

    Update file paths

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var http = require('http');
-var fs = require('fs');
+    fs = require('fs');
+    url = require('url');
 
 http.createServer(function (req, res) {
-  fs.readFile('address.html', function(err, data) {
+  //use the url to parse the requested url and get the image name
+  var query = url.parse(req.url,true).query;
+      pic = query.image;
+
+  fs.readFile('public/address.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
-  fs.readFile('pin-test.png', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'image/png'});
-    res.write(data);
-    return res.end();
+  fs.readFile('/public/images' + pic, function (err, content) {
+      if (err) {
+          res.writeHead(400, {'Content-type':'text/html'})
+          console.log(err);
+          res.end("No such image");
+      } else {
+          //specify the content type in the response will be an image
+          res.writeHead(200,{'Content-type':'image/png'});
+          res.end(content);
+      }
   });
 }).listen(8080);
