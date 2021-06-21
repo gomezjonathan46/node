878e5195d5ed503a231a8fda85dd519570a38415
Jon Gomez <jgomez@brownsteingroup.com>
2021-06-21 18:09:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/', router);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
